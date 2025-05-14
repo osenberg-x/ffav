568437a4ab00ab284228404353080d2005af8416
xvs <osenberg@qq.com>
2025-05-14 23:43:09 +0800

    feat: added avformat packages dir

---
+++ packages/avformat/src/lib.rs
@@ -0,0 +1,14 @@
+struct AVFormatContext {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
